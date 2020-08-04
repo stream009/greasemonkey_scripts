f9427f0c8a01c4e9ccc029b1bc01bdc8f7e36f9c
dn7 <61009930+dn7@users.noreply.github.com>
2020-08-04 17:23:13 +0300

    Update MemriseDisableAutoAccept.user.js
    Memrise seems to have changed the addresses of their lessons from `https://www.memrise.com/course/*/garden/*` to `https://app.memrise.com/course/*/garden/*` , and the same for review (changing `www` to `app`). This means the script is not called anymore. This change worked for me.

--- MemriseDisableAutoAccept.user.js
+++ MemriseDisableAutoAccept.user.js
@@ -4,6 +4,8 @@
 // @description    Disable auto accept feature on Memrise typing test
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
+// @match          https://app.memrise.com/course/*/garden/*
+// @match          https://app.memrise.com/garden/review/*
 // @version        1.1.0
 // @updateURL      https://github.com/stream009/greasemonkey_scripts/raw/master/MemriseDisableAutoAccept.user.js
 // @downloadURL    https://github.com/stream009/greasemonkey_scripts/raw/master/MemriseDisableAutoAccept.user.js
